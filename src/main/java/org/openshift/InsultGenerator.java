de688a49670b2f08111728f9ca95ecf070e70205
balo-zsolt <balozsoltmfd@yahoo.de>
2019-08-21 14:24:55 +0300

    Syntax error fixed

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class InsultGenerator {
-	public Strin generateInsult() {
+	public String generateInsult() {
 		String vowels = "AEIOU";
 		String article = "an";
 		String theInsult = "";
